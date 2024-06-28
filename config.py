4805e78f15de3497d88264d6d200ab5fbfada4e3
yong-del <uxjagle401@mailpod.site>
2024-06-28 10:35:24 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,29 @@
+ip_version_priority = "ipv6"
+source_urls = [
+    "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
+    "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
+    "http://120.79.4.185/new/mdlive.txt",
+    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
+    "https://live.zhoujie218.top/tv/iptv4.txt",
+    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V6.txt",
+    "https://live.zhoujie218.top/tv/iptv6.txt",
+    "https://tv.youdu.fan:666/live/",
+    "http://175.178.251.183:6689/livv.txt",
+    "https://fm1077.serv00.net/litv.txt",
+    "https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt",
+    "https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1715581924111/live1.txt",
+    "http://rihou.cc:88/hccx/zb.txt",
+    "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/hd.txt",
+    "https://cdn.jsdelivr.net/gh/YueChan/live@main/IPTV.m3u",
+    "https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
+    "https://fm1077.serv00.net/SmartTV.m3u",
+    "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt"
+]
+url_blacklist = [
+    "epg.pw/stream/",
+    "103.40.13.71:12390",
+    "[2409:8087:1a01:df::4077]/PLTV/",
+    "8.210.140.75:68",
+    "154.12.50.54",
+    "yinhe.live_hls.zte.com"
+]
