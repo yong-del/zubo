64a14d9a28ab35680b8d484a12a5db599972e6c6
yong-del <uxjagle401@mailpod.site>
2024-07-05 10:21:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 ip_version_priority = "ipv6"
 source_urls = [
     "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
+    "https://raw.githubusercontent.com/yong-del/searchZUBO/main/陕西电信.txt",
+    "https://raw.githubusercontent.com/yong-del/searchZUBO/main/北京联通.txt"，
     "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
     "http://120.79.4.185/new/mdlive.txt",
     "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
@@ -17,9 +19,7 @@ source_urls = [
     "https://cdn.jsdelivr.net/gh/YueChan/live@main/IPTV.m3u",
     "https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
     "https://fm1077.serv00.net/SmartTV.m3u",
-    "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
-    "https://raw.githubusercontent.com/yong-del/searchZUBO/main/陕西电信.txt",
-    "https://raw.githubusercontent.com/yong-del/searchZUBO/main/北京联通.txt"
+    "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt"   
 ]
 url_blacklist = [
     "epg.pw/stream/",
