d2a78b27b12ddc19279d3bf0333673db6c326509
yong-del <yongshou7@proton.me>
2024-08-02 17:27:46 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,56 @@
 ip_version_priority = "ipv6"
+
 source_urls = [
+    #"https://raw.githubusercontent.com/redrainl/iptv/main/speedtest/zubo_fofa.txt",
+    #"https://jihulab.com/ss3234741/sj/raw/2a8f0afa421a98836bc891698446c7271c2f32f3/lib/live2.txt",
+    #"https://gitcode.net/ygbh66/666/raw/master/x.txt",
     "https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/ipv6.m3u"
-    #"https://raw.githubusercontent.com/lucheng7996/TE/main/outfiles/shaanxi_ctcc.txt",
-    #"https://raw.githubusercontent.com/yong-del/searchZUBO/main/北京联通.txt"
-   
+    #"https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
+    #"http://120.79.4.185/new/mdlive.txt",
+    #"https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
+    #"https://live.zhoujie218.top/tv/iptv6.txt",
+    #"https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
+    #"https://raw.githubusercontent.com/vbskycn/iptv/master/tv/hd.txt",
+    #"https://cdn.jsdelivr.net/gh/YueChan/live@main/IPTV.m3u",
+    #"https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
+    #"https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
+    #"https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
+    #"https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt"
 ]
+
 url_blacklist = [
     "epg.pw/stream/",
     "103.40.13.71:12390",
     "[2409:8087:1a01:df::4077]/PLTV/",
     "8.210.140.75:68",
     "154.12.50.54",
-    "yinhe.live_hls.zte.com"
+    "yinhe.live_hls.zte.com",
+    "8.137.59.151",
+    "[2409:8087:7000:20:1000::22]:6060",
+    "histar.zapi.us.kg",
+    "www.tfiplaytv.vip",
+    "dp.sxtv.top",
+    "111.230.30.193",
+    "148.135.93.213:81",
+    "live.goodiptv.club",
+    "iptv.luas.edu.cn"
+]
+
+announcements = [
+    {
+        "channel": "公告",
+        "entries": [
+            {"name": "更新日期", "url": "https://liuliuliu.tv/api/channels/1997/stream", "logo": "http://175.178.251.183:6689/LR.jpg"},
+            {"name": "None", "url": "https://liuliuliu.tv/api/channels/233/stream", "logo": "http://175.178.251.183:6689/LR.jpg"}
+  ]
+    }
+]
+
+epg_urls = [
+    "https://live.fanmingming.com/e.xml",
+    "http://epg.51zmt.top:8000/e.xml",
+    "http://epg.aptvapp.com/xml",
+    "https://epg.pw/xmltv/epg_CN.xml",
+    "https://epg.pw/xmltv/epg_HK.xml",
+    "https://epg.pw/xmltv/epg_TW.xml"
 ]
